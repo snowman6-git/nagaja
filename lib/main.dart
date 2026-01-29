92a7d8d0548987d827260e28df773c1636d1b395
snowman6-git <chldlfgks55@gmail.com>
2026-01-29 22:59:47 +0900

    기초코드, 인터넷 연결 여부 확인과 다이얼로그

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -33,15 +34,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
+  bool _isDialogOpen = false;
+
+
   @override
   void initState() {
     super.initState();
     _checkInternetConnection();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
   Future<void> _checkInternetConnection() async{
-    var connectivityResult = await _connectivity.checkConnectivity();W
+    var connectivityResult = await _connectivity.checkConnectivity();
+  }
+  void _updateConnectionStatus(List<ConnectivityResult> result){
+    _handleConnectionStatus(result);
+  }
+  void _handleConnectionStatus(List<ConnectivityResult> result){
+    for (var element in result){
+      //모바일 데이터 or wifi
+      if (element == ConnectivityResult.mobile || element == ConnectivityResult.wifi){
+        if(_isDialogOpen){
+          Navigator.of(context).pop();
+          _isDialogOpen = false;
+        }
+        return;
+      //연결이 안됐다면
+      } else {
+        //다이얼로그 열기
+        _showOfflineDialog();
+      }
+    }
   }
 
+  void _showOfflineDialog(){
+    if (!_isDialogOpen && mounted){
+      showDialog(
+          context: context,
+          builder: (BuildContext context){
+            _isDialogOpen = true;
+            return AlertDialog(
+              title: const Text("나가자"),
+              content: const Text(
+                  "지금은 인터넷이 연결되어있지 않아요\n"
+                  "몇몇 기능이 제한되고 버스 시간이 실시간 반영되지 않을 수 있어요."
+              ),
+              actions: [
+                TextButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                      _isDialogOpen = false;
+                    },
+                    child: const Text("이해했어요")
+                )
+              ],
+            );
+          }
+      ).then((_) => _isDialogOpen = false);
+    }
+  }
+  @override
+  void dispose(){
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
